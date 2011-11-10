c3b6abb1f124bffbc88c301e3d4fc7b9f58a0098
Jeremy Mates <jmates@sial.org>
2011-11-10 20:50:05 +0000

    Use Music::Chord::Note less, add chord_inv method to calculate inversions for a given pitch set, some tests thereof.

--- lib/Music/Chord/Positions.pm
+++ lib/Music/Chord/Positions.pm
@@ -3,13 +3,46 @@ package Music::Chord::Positions;
 use strict;
 use warnings;
 
-use parent qw(Music::Chord::Note);
+use Carp qw(croak);
+use List::Util qw(min max);
 
 our $VERSION = '0.01';
 
+my $DEG_IN_SCALE = 12;
+
 sub new {
   my ($class) = @_;
-  $class->SUPER::new();
+  bless {}, $class;
+}
+
+sub chord_inv {
+  my ( $self, $pitch_set, %params ) = @_;
+  croak "chord_inv requires a pitch set"
+    unless defined $pitch_set and ref $pitch_set eq 'ARRAY';
+
+  my $max_pitch   = max(@$pitch_set);
+  my $next_octave = $max_pitch + $DEG_IN_SCALE - $max_pitch % $DEG_IN_SCALE;
+
+  my @inversions;
+  for my $i ( 0 .. $#$pitch_set - 1 ) {
+    # Inversions simply flip lower notes up above the highest pitch in
+    # the original pitch set.
+    push @inversions,
+      [
+      @$pitch_set[ $i + 1 .. $#$pitch_set ],
+      map { $next_octave + $_ } @$pitch_set[ 0 .. $i ]
+      ];
+
+    # Normalize to "0th" register if lowest pitch is an octave+ out    TODO
+    # not sure if I want this change, as it is display/output related.       :/
+    #   my $min_pitch = min( @{ $inversions[-1] } );
+    #   if ( $min_pitch >= $DEG_IN_SCALE ) {
+    #     my $offset = $min_pitch - $min_pitch % $DEG_IN_SCALE;
+    #     $_ -= $offset for @{ $inversions[-1] };
+    #   }
+  }
+
+  return @inversions;
 }
 
 # TODO options for voices (could be > = < than pitch set), whether to
@@ -22,39 +55,68 @@ sub new {
 #
 # And whether chords suit SATB vocal ranges or not (or how well?) hmm.
 sub chord_pos {
-  my ( $self, $chord ) = @_;
+  my ( $self, $pitch_set, %params ) = @_;
 
-  my @pitches = $self->SUPER::chord_num($chord);
-  return @pitches;
+  croak "chord_pos requires a pitch set"
+    unless defined $pitch_set and ref $pitch_set eq 'ARRAY';
+
+  # DBG until I figure out how to handle 3 or 5 or what (unset would
+  # imply to take the # of "voices" from the chord?)
+  $params{'-voices'} = 4;
+  $params{'-factor'} = 3;    # TODO need better name
+
+  if ( @$pitch_set > $params{'-voices'} ) {
+    # as would need to figure out what are the permitted doublings, etc.
+    croak
+      "case where pitches in chord exceedes allowed voices not implemented\n";
+  }
+
+  my $max_interval = max @$pitch_set;
+
+  # How high above fundamental to allow
+  my $voicing_limit =
+    ( $max_interval + $DEG_IN_SCALE - $max_interval % $DEG_IN_SCALE ) *
+    $params{'-factor'};
+
+  if ( @$pitch_set < $params{'-voices'} ) {
+    # TODO if delta 1 easy just add octave, what to do if 5 voices for a 3
+    # pitch chord?
+  }
+
+  # Calculate different positions (revoicing, same fundamental tone).
+  my @revoicings;
+
+  return @revoicings;
 }
-
-# TODO new class for PitchSet return, then means to filter (open,
-# closed, vocalizable) or format (MIDI, lilypond, etc) those sets
-# instead of polluting call to chord_pos?
 
 1;
 __END__
 
 =head1 NAME
 
-Music::Chord::Positions - Perl extension for blah blah blah
+Music::Chord::Positions - generates various chord voicings
 
 =head1 SYNOPSIS
 
   use Music::Chord::Positions;
-  blah blah blah
+  my $mcp = Music::Chord::Positions->new();
+
+  TODO
 
 =head1 DESCRIPTION
 
-Stub documentation for Music::Chord::Positions, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+Given a set of semitone intervals (as an array ref), generates alternate
+voicings for those pitches in the modern Western system. The voicings
+(TODO will) include closed or open position variations (up to an upper
+limit), and inversions. The pitch set may be specified manually, or the
+B<chord_num> method of L<Music::Chord::Note> used to derive a pitch set
+from a named chord.
 
 =head1 SEE ALSO
 
 L<Music::Chord::Note>
+
+B<Theory of Harmony> by Arnold Schoenberg
 
 =head1 AUTHOR
 
@@ -62,7 +124,7 @@ Jeremy Mates E<lt>jmates@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 by Jeremy Mates
+Copyright (C) 2011 Jeremy Mates
 
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself, either Perl version 5.14 or, at
--- t/Music-Chord-Positions.t
+++ t/Music-Chord-Positions.t
@@ -1,18 +1,41 @@
 use strict;
 use warnings;
 
-use Test::More tests => 4;
-BEGIN { use_ok('Music::Chord::Positions') }
+use Test::More tests => 5;
 
-can_ok( 'Music::Chord::Positions', qw/new chord_pos/ );
+BEGIN { use_ok('Music::Chord::Positions') }
+can_ok( 'Music::Chord::Positions', qw/new chord_inv chord_pos/ );
 
 my $mcp = Music::Chord::Positions->new();
+isa_ok( $mcp, 'Music::Chord::Positions' );
 
-# Check that parent behaves as expected, breakage here would indicate
-# Music::Chord::Note has changed interface since 0.0.6.
-{
-  isa_ok( $mcp, 'Music::Chord::Note' );
+# 5th should generate 1st and 2nd inversions
+my @inversions = $mcp->chord_inv( [ 0, 4, 7 ] );
+is_deeply(
+  \@inversions,
+  [ [ 4, 7, 12 ], [ 7, 12, 16 ] ],
+  'check inversions of 5th'
+);
 
-  is( join( q{,}, $mcp->chord_num() ),
-    '0,4,7', 'check chord_num from parent' );
-}
+# 7th - 1st, 2nd, and 3rd inversions
+@inversions = $mcp->chord_inv( [ 0, 4, 7, 11 ] );
+is_deeply(
+  \@inversions,
+  [ [ 4, 7, 11, 12 ], [ 7, 11, 12, 16 ], [ 11, 12, 16, 19 ] ],
+  'check inversions of 7th'
+);
+
+# 9th - spans octave! (maybe also 15th, 17th to make sure 2x octaves
+# does the right thing) - TODO generate lilypond from output to confirm
+# things look correct, figure out whether or not will normalize to base
+# register if min > $D_I_S.
+@inversions = $mcp->chord_inv( [ 0, 4, 7, 11, 14 ] );
+#is_deeply(
+#  \@inversions,
+#  [ [ 4, 7, 11,  12 ], [ 7, 11, 12, 16 ], [ 11, 12, 16, 19 ], [] ],
+#  'check inversions of 7th'
+#);
+#use Data::Dumper; diag Dumper \@inversions;
+#
+# TODO also 15th, 17th chords to make sure 2x octave spans handled
+# correctly.
