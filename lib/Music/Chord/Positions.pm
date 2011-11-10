6352a7a5e9c5f7bebcd42053397fc5391133d654
Jeremy Mates <jmates@sial.org>
2011-11-10 05:19:58 +0000

    From h2xs(1) plus some tweaks.

---
+++ lib/Music/Chord/Positions.pm
@@ -0,0 +1,78 @@
+package Music::Chord::Positions;
+
+use strict;
+use warnings;
+
+our @ISA = qw();
+
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Music::Chord::Positions - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Music::Chord::Positions;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Music::Chord::Positions, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head1 HISTORY
+
+=over 8
+
+=item 0.01
+
+Original version; created by h2xs 1.23 with options
+
+  -ACX
+	-b
+	5.8.0
+	--use-new-tests
+	--skip-exporter
+	-n
+	Music::Chord::Positions
+
+=back
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Jeremy Mates E<lt>jmates@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by Jeremy Mates
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.14.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
