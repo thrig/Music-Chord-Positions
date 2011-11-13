ad650161e9f539c36b508e83ecbd342c628156db
Jeremy Mates <jmates@sial.org>
2011-11-13 03:35:05 +0000

    Documentation additions, spelling fixes, new parameters to influence voicings generated.

--- lib/Music/Chord/Positions.pm
+++ lib/Music/Chord/Positions.pm
@@ -36,7 +36,7 @@ sub chord_inv {
       ];
 
     # Normalize to "0th" register if lowest pitch is an octave+ out
-    # TODO this is a output/display issue, move to formating routines if
+    # TODO this is a output/display issue, move to formatting routines if
     # write those.
     #   my $min_pitch = min( @{ $inversions[-1] } );
     #   if ( $min_pitch >= $DEG_IN_SCALE ) {
@@ -48,16 +48,6 @@ sub chord_inv {
   return @inversions;
 }
 
-# TODO options for voices (could be > = < than pitch set), whether to
-# allow doubling and if so on what degrees (root, 5th, 3rd, ..., all)
-# doubling is allowed. Ohh, also top limit for voicings, as computers
-# could numerate voices out to crazy spans (3 would be a good limit for
-# 5ths, as Theory of Harmony only extends open position to 2+5th, might
-# need more for 13th chords). Also means to limit by 'closed' or 'open'
-# position definition from ToH, etc.
-#
-# And whether chords suit SATB vocal ranges or not (or how well?) hmm.
-# That would be a filter question, not generation or output?
 sub chord_pos {
   my ( $self, $pitch_set, %params ) = @_;
   croak "pitch set reference required"
@@ -80,7 +70,7 @@ sub chord_pos {
   if ( exists $params{'-voices'} ) {
     if ( @$pitch_set > $params{'-voices'} ) {
       die
-        "case where pitches in chord exceedes allowed voices not implemented";
+        "case where pitches in chord exceeds allowed voices not implemented";
     }
   } else {
     $params{'-voices'} = @$pitch_set;
@@ -96,7 +86,7 @@ sub chord_pos {
     my $doubled_count = $params{'-voices'} - @ps;
     die "multiple extra voices not implemented" if $doubled_count > 1;
 
-    # Double lowest pitch in octave above higest pitch C E G -> C E G C
+    # Double lowest pitch in octave above highest pitch C E G -> C E G C
     push @ps, $next_register + $ps[0];
   }
 
@@ -122,11 +112,16 @@ sub chord_pos {
       for my $p (@chord) {
         $harmeq{ $p % $DEG_IN_SCALE }++;
       }
-      # Not enough unique pitches
-      next if keys %harmeq < $unique_pitch_count;
-      # Disallow doubled notes (excepting root)
-      for my $k ( grep { $_ != $min_pitch_norm } keys %harmeq ) {
-        next TOPV if $harmeq{$k} > 1;
+
+      unless ( exists $params{'-disable-upc'} and $params{'-disable-upc'} ) {
+        next if keys %harmeq < $unique_pitch_count;
+      }
+
+      unless ( exists $params{'-all-doublings'}
+        and $params{'-all-doublings'} ) {
+        for my $k ( grep { $_ != $min_pitch_norm } keys %harmeq ) {
+          next TOPV if $harmeq{$k} > 1;
+        }
       }
 
       # Nix any identical chord voicings (c e g == c' e' g')
@@ -140,17 +135,18 @@ sub chord_pos {
       push @revoicings, \@chord;
     }
 
-    # Increment any lower voices if top voice(s) maxxed out
+    # Increment any lower voices if top voice(s) maxed out
     for my $i ( reverse 1 .. $#voice_iters ) {
       if ( $voice_iters[$i] > $voice_max[$i] ) {
         $voice_iters[ $i - 1 ]++;
       }
     }
 
-    # Constrain root to just octaves of min pitch
-    while (
-      $potentials[ $voice_iters[0] ] % $DEG_IN_SCALE != $min_pitch_norm ) {
-      $voice_iters[0]++;
+    unless ( exists $params{'-any-root'} and $params{'-any-root'} ) {
+      while (
+        $potentials[ $voice_iters[0] ] % $DEG_IN_SCALE != $min_pitch_norm ) {
+        $voice_iters[0]++;
+      }
     }
 
     # Reset higher voices to close positions above lower voices
@@ -179,6 +175,9 @@ Music::Chord::Positions - generate various chord inversions and voicings
   my @inverses = $mcp->chord_inv([0,4,7]);
   my @voicings = $mcp->chord_pos([0,4,7]);
 
+Converting the resulting lists of pitch sets to something useful left as
+an exercise to the user.
+
 =head1 DESCRIPTION
 
 Utility methods for generating inversions or chord voicing variations of
@@ -189,21 +188,99 @@ semitone intervals that could constitute some sort of chord.
   [0, 3, 7]      # minor      C  D# G
   [0, 4, 7, 11]  # Major 7th  C  E  G  B
 
-The pitch set may be specified manually, or the B<chord_num> method of
-L<Music::Chord::Note> used to derive a pitch set from a named chord. So
-the following should result in identical lists of inversions of C minor.
+Or whatever. The pitch set may be specified manually, or the
+B<chord_num> method of L<Music::Chord::Note> used to derive a pitch set
+from a named chord.
 
+  # These result in the same output from chord_inv()
   my @ps = (0,3,7);
   $mcp->chord_inv(\@ps);
 
   use Music::Chord::Note;
   $mcp->chord_inv([ Music::Chord::Note->new->chord_num('Cm') ]);
 
+=head1 METHODS
+
+Assuming the B<new> class method has been used to create a
+C<Music::Chord::Positions> object, the following methods are available
+from that object:
+
+=over 4
+
+=item B<chord_inv>( I<pitch set reference> )
+
+Generates inversions of the pitch set, returns a list of pitch sets
+(list of array references). The order of the results will be 1st
+inversion, 2nd inversion, etc.
+
+No transposition is performed, so inversions of 9ths or larger may
+result in a chord in a register above the original. If this is a
+problem, decrement the semitones in the pitch set by 12 or whatever.
+
+=item B<chord_pos>( I<pitch set reference>, I<list of optional parameters> ... )
+
+Generate different voicings of a different chord, by default in
+registers two above the base. Only voicings where the root remains in
+the root will be considered; chords that do not represent all pitches in
+the pitch set or chords that double non-root pitches will be excluded.
+Chords with intervals greater than 19 semitones (octave+fifth) between
+adjacent pitches will also be excluded, as will transpositions of the
+same voicing into higher registers.
+
+The B<chord_pos> method can be influenced by the following parameters:
+
+=over 4
+
+=item B<-all-doublings> I<1>
+
+If set and true, allows doublings on all pitches, not just the default
+of the root pitch.
+
+=item B<-any-root> I<1>
+
+If set and true, allows the root pitch of the voicing to be any member
+of the original pitch set, not just the lowest of that set.
+
+=item B<-disable-upc> I<1>
+
+If set and true, disables the unique pitch check. That is, voicings will
+be allowed with fewer pitches than in the original pitch set.
+
+=item B<-iinterval-max> I<19>
+
+Largest adjacent pitch interval allowed between two pitches of a
+voicing, in semitones.
+
+=item B<-octaves> I<2>
+
+How far above the register of the chord to generate voicings in. If set
+to a large value, the B<-iinterval-max> value should likely also be
+increased.
+
+=item B<-voices> I<depends on pitch set passed>
+
+Use this to customize the number of voices in the different chord
+voicings. At present, only one extra voice above the number of voices
+in the pitch set is implemented. Mostly to support SATB for three-
+pitch chords, in which case the root pitch will be doubled:
+
+  $mcp->chord_pos([0,4,7], -voices => 4);
+
+=back
+
+The chord voicings allowed by the default options may still not suit
+certain musical styles; for example, in SATB chorales, the bass alone is
+allowed to drift far from the other voices, but not both the bass and
+tenor from the upper voices.
+
+=back
+
 =head1 SEE ALSO
 
 L<Music::Chord::Note>
 
-B<Theory of Harmony> by Arnold Schoenberg
+B<Theory of Harmony> by Arnold Schoenberg (whose chord voicing exercise
+prompted this exercise in coding).
 
 =head1 AUTHOR
 
