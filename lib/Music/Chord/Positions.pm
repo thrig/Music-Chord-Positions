89e312d3692cf5210d4715af4b8f2417710c742d
Jeremy Mates <jmates@sial.org>
2011-12-14 15:36:59 +0000

    Version bump for new progressions script.

--- lib/Music/Chord/Positions.pm
+++ lib/Music/Chord/Positions.pm
@@ -8,7 +8,7 @@ use Exporter ();
 use List::MoreUtils qw(all uniq);
 use List::Util qw(max min);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 our ( @ISA, @EXPORT_OK, %EXPORT_TAGS );
 @ISA = qw(Exporter);
