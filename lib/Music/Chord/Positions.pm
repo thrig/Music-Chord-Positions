2b82ce52a7d61d1db1b1203c4676d523456390bf
Jeremy Mates <jmates@cpan.org>
2015-02-04 02:31:00 +0000

    Relicense under artistic_2.

--- lib/Music/Chord/Positions.pm
+++ lib/Music/Chord/Positions.pm
@@ -541,10 +541,9 @@ thrig - Jeremy Mates (cpan:JMATES) C<< <jmates at cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011-2013 Jeremy Mates
+Copyright (C) 2011-2015 Jeremy Mates
 
-This library is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself, either Perl version 5.16 or, at
-your option, any later version of Perl 5 you may have available.
+This module is free software; you can redistribute it and/or modify it
+under the Artistic License (2.0).
 
 =cut
